c1cc17e7e00d88389eed4b4f01a2900e98e2c78c
Chad Gresham <bxxdybagz@gmail.com>
2019-12-18 00:35:34 -0500

    Server.js done, strong compenent building and rough draft in progress...

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const nameOfApp = "my-life";
 /*Set global constant to use third-party library express*/
 const express = require("express");
 
+/*Configure our port by seting it to a variable*/
+const PORT = process.env.PORT || 3001;
+
 /*Set our app to call express everytime this variable is called*/
 const app = express();
 
 
-// Start the API server
+/*Start the API server*/
 app.listen(PORT, function() {
-    console.log(`What's up 🌎  ==> ${nameOfApp} Sever is now listening on PORT ${PORT}!`);
+    console.log(`What's up 🌎  ==> ${nameOfApp} Server is now listening on PORT ${PORT}!`);
   });
   

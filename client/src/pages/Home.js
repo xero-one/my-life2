1702762acb3390200c226899712d902984083c8b
Chad Gresham <bxxdybagz@gmail.com>
2020-01-22 02:21:24 -0500

    test commits not working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import NavBar from "../components/navBar/navBar.js";
 
 const element = <h1>This is a test</h1>;
 console.log(element);
@@ -7,10 +8,12 @@ console.log(element);
 class Home extends Component {
 render(){
     return(
+        <NavBar>
         <div className={"text-center"}>
              <a href="/login"><button>Sign In</button></a>
             <a href="/signup"><button>Sign Up</button></a>
         </div>
+        </NavBar>
     )
 }
 }
